561e122aca4e5af385469407810c1d3dd3d8b2bf
Alvin <alvinhewittalvin@gmail.com>
2019-04-23 19:36:22 -0700

    pull from firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,4 +13,19 @@ $(document).ready(function() {
 	firebase.initializeApp(config);
 
 	var database = firebase.database();
+
+	// when form submit clicked, take in value from form
+	// push it to the firebase
+
+	// firebase on("child_added") change
+	// pull the object down
+	// display it on the html table with <tr><td></td><tr>
+	database.ref().on("child_added", function(data) {
+		var name = $("<td>").text(item.name.trim());
+		var title = $("<td>").text(item.title.trim());
+		var startDate = $("<td>").text(item.startDate);
+		var rate = $("<td>").text(item.rate);
+		var month = 
+		$(".display").append($("<tr>").append([name, title, startDate, month, rate, pay]));
+	});
 });
